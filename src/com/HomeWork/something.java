ec810920e3f5ecdbbd76770b3b9b15cf3ed18171
VadymDemishev <vadimdemishev@gmail.com>
2018-03-27 15:48:39 +0300

    Update something.java
    version-3

--- src/com/HomeWork/something.java
+++ src/com/HomeWork/something.java
@@ -3,7 +3,7 @@ package com.HomeWork;
 public class something {
 
     public static void main (String[] args) {
-        System.out.println("Hello World-1");
+        System.out.println("Hello World-3");
 
     }
 }
